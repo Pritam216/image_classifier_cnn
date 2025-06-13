78167b942a592541d3b406243b9701967ad993aa
Pritam <kumarroypritam2@gmail.com>
2025-06-14 00:59:53 +0530

    commit done

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from predict_page import image_classifier_page 
 
-st.set_page_config(page_title="My Awesome App", layout="centered")
+# st.set_page_config(page_title="My Awesome App", layout="centered")
 # st.sidebar.title("Navigation")
 image_classifier_page()
